7a5162c226b609cfd87e976258a236c45163c92c
Eyad Maged <eyadmaged49009@gmail.com>
2025-06-16 22:13:17 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+number1 = input("enter your first number:")
+number2 = input("enter your sec number:")
+addition = (int(number1)+int(number2))
+subtraction = (int(number1)-int(number2))
+multiplication= (int(number1)*int(number2))
+division= (int(number1)/int(number2))
+power = (int(number1)**int(number2))
+
+
+print(f"addition = {addition}")
+print(f"subtraction = {subtraction}")
+print(f"multiplication = {multiplication}")
+print(f"division = {division}")
+print(f"power = {power}")
+
