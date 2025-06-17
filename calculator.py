6520f56f85e9a2ff8819e63972cf662aa4730007
Eyad Maged <eyadmaged49009@gmail.com>
2025-06-17 21:15:14 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
-number1 = input("enter your first number:")
-number2 = input("enter your sec number:")
+number1 = input("Enter Your First Number:")
+number2 = input("Enter Your Sec Number:")
 addition = (int(number1)+int(number2))
 subtraction = (int(number1)-int(number2))
 multiplication= (int(number1)*int(number2))
@@ -7,9 +7,8 @@ division= (int(number1)/int(number2))
 power = (int(number1)**int(number2))
 
 
-print(f"addition = {addition}")
-print(f"subtraction = {subtraction}")
-print(f"multiplication = {multiplication}")
-print(f"division = {division}")
-print(f"power = {power}")
-
+print(f"Addition = {addition}")
+print(f"Subtraction = {subtraction}")
+print(f"Multiplication = {multiplication}")
+print(f"Division = {division}")
+print(f"Power = {power}")
